1f25cd6e4d137e429da47aeb388574336f4678e4
KaweVK <victorkawe07@gmail.com>
2025-05-15 10:06:27 -0300

    Item Controller findById

--- src/main/java/com/uni/vendas/controllers/ItemController.java
+++ src/main/java/com/uni/vendas/controllers/ItemController.java
@@ -1,7 +1,12 @@
 package com.uni.vendas.controllers;
 
+import com.uni.vendas.data.dto.ItemDTO;
 import com.uni.vendas.services.ItemService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,14 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    @GetMapping
+    @RequestMapping(value = "/{id}",
+        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
+    )
+    public ItemDTO getItemById(@PathParam("id") Long id) {
+        var item = itemService.findById(id);
 
+        return item;
+    }
 
 }
