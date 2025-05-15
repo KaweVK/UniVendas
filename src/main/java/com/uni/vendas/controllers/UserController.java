8992401fe68eed313e638b880de406fbabaa6248
KaweVK <victorkawe07@gmail.com>
2025-05-14 23:39:07 -0300

    UserController Complete

--- src/main/java/com/uni/vendas/controllers/UserController.java
+++ src/main/java/com/uni/vendas/controllers/UserController.java
@@ -4,9 +4,9 @@ package com.uni.vendas.controllers;
 import com.uni.vendas.data.dto.UserDTO;
 import com.uni.vendas.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,29 +16,52 @@ public class UserController {
     @Autowired
     private UserServices userServices;
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
+    )
     public UserDTO findById(@PathVariable("id") Long id) {
         var person = userServices.findById(id);
 
         return person;
     }
 
-    @GetMapping()
+    @GetMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
+    )
     public List<UserDTO> findAll() {
-        var persons = userServices.findAll();
+        var user = userServices.findAll();
 
-        return persons;
+        return user;
     }
 
 
-    public UserDTO createPerson(UserDTO dto) {
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
+    )
+    public UserDTO createPerson(@RequestBody UserDTO dto) {
+        var user = userServices.crateUser(dto);
 
+        return user;
     }
 
-    public Class<?> deletePerson(Long id) {
+
+    @DeleteMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
+    )
+    public ResponseEntity<?> deletePerson(@PathVariable("id") Long id) {
+        userServices.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 
-    public Class<?> updatePerson(UserDTO dto) {
-        return null;
+    @PutMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE}
+    )
+    public UserDTO updatePerson(@RequestBody UserDTO dto) {
+        return userServices.updateUser(dto);
     }
 }
