b35d0c038ebf2113cef127c9939fe5b4d106e49a
KaweVK <victorkawe07@gmail.com>
2025-05-19 15:33:46 -0300

    Created interface and operation of get methods

---
+++ src/main/java/com/uni/vendas/controllers/docs/UserControllerDocs.java
@@ -0,0 +1,48 @@
+package com.uni.vendas.controllers.docs;
+
+import com.uni.vendas.data.dto.v1.UserDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+public interface UserControllerDocs {
+
+    @Operation(summary = "Find a User",
+            description = "Find a User with user's Id",
+            tags = {"Users"},
+            responses = {
+                @ApiResponse(description = "Sucess", responseCode = "200", content =
+                    @Content(schema = @Schema(implementation = UserDTO.class))
+                ),
+                @ApiResponse(description = "No content", responseCode = "204", content = @Content),
+                @ApiResponse(description = "Bad request", responseCode = "400", content = @Content),
+                @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    UserDTO findById(@PathVariable("Id") Long id);
+
+    @Operation(summary = "Find all Users",
+            description = "Find all Users",
+            tags = {"Users"},
+            responses = {
+                @ApiResponse(description = "Sucess", responseCode = "200", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, array = @ArraySchema(schema = @Schema(implementation = UserDTO.class)))
+                }),
+                @ApiResponse(description = "No content", responseCode = "204", content = @Content),
+                @ApiResponse(description = "Bad request", responseCode = "400", content = @Content),
+                @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
+    List<UserDTO> findAll();
+
+}
