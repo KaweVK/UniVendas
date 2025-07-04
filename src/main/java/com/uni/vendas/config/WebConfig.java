ff046221d4fd9d6e72e6c33fda1175ebbd91e2a1
KaweVK <victorkawe07@gmail.com>
2025-07-03 21:55:43 -0300

    Removed yaml on web config

--- src/main/java/com/uni/vendas/config/WebConfig.java
+++ src/main/java/com/uni/vendas/config/WebConfig.java
@@ -14,13 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
                 .ignoreAcceptHeader(false).useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML)
-                .mediaType("yaml", MediaType.APPLICATION_YAML);
-
-//        configurer.favorParameter(true).parameterName("mediaType")
-//                .ignoreAcceptHeader(true).useRegisteredExtensionsOnly(false)
-//                .defaultContentType(MediaType.APPLICATION_JSON)
-//                .mediaType("json", MediaType.APPLICATION_JSON)
-//                .mediaType("xml", MediaType.APPLICATION_XML);
+                .mediaType("xml", MediaType.APPLICATION_XML);
     }
 }
