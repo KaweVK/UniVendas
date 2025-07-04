36729e08ec567f1e625a1f4ec86435de355f0711
KaweVK <victorkawe07@gmail.com>
2025-07-03 21:54:35 -0300

    New ItemRepository

--- src/main/java/com/uni/vendas/repository/ItemRepository.java
+++ src/main/java/com/uni/vendas/repository/ItemRepository.java
@@ -3,5 +3,11 @@ package com.uni.vendas.repository;
 import com.uni.vendas.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ItemRepository extends JpaRepository<Item, UUID> {
+
+    Optional<Item> findByIdAndNameAndDescriptionAndAmountAndPrice(UUID id, String name, String description, Long amount, BigDecimal price);
 }
