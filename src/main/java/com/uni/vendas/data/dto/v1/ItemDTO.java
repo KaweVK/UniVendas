04f1c4d96445e812e6fb1f657730e416d5209db7
KaweVK <victorkawe07@gmail.com>
2025-07-03 21:56:17 -0300

    New ItemDTO

--- src/main/java/com/uni/vendas/data/dto/v1/ItemDTO.java
+++ src/main/java/com/uni/vendas/data/dto/v1/ItemDTO.java
@@ -1,82 +1,31 @@
 package com.uni.vendas.data.dto.v1;
 
-
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import org.springframework.hateoas.RepresentationModel;
+import com.uni.vendas.models.Item;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-import java.util.Objects;
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @JsonPropertyOrder({"id", "name", "description", "amount", "price"})
-public class ItemDTO extends RepresentationModel<ItemDTO> {
+public record ItemDTO(
+        UUID id,
+        @NotBlank(message = "Name cannot be blank")
+        String name,
+        @NotBlank(message = "Description cannot be blank")
+        String description,
+        @NotNull(message = "Price is required")
+        Long amount,
+        @NotNull(message = "Price is required")
+        BigDecimal price) {
 
-    private static final long serialVersionUID = 1L;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String name;
-    private String description;
-    private Long amount;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private double price;
-
-    public ItemDTO() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Long amount) {
-        this.amount = amount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        ItemDTO itemDTO = (ItemDTO) o;
-        return Double.compare(getPrice(), itemDTO.getPrice()) == 0 && Objects.equals(getId(), itemDTO.getId()) && Objects.equals(getName(), itemDTO.getName()) && Objects.equals(getDescription(), itemDTO.getDescription()) && Objects.equals(getAmount(), itemDTO.getAmount());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getId(), getName(), getDescription(), getAmount(), getPrice());
+    public Item MapToItem() {
+        Item item = new Item();
+        item.setName(name);
+        item.setDescription(description);
+        item.setAmount(amount);
+        item.setPrice(price);
+        return item;
     }
 }
