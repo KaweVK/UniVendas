103eeda9bdb099f94cbd9c1b4bafe01248409901
KaweVK <victorkawe07@gmail.com>
2025-07-03 22:12:02 -0300

    Changes on UserDTO

--- src/main/java/com/uni/vendas/data/dto/v1/UserDTO.java
+++ src/main/java/com/uni/vendas/data/dto/v1/UserDTO.java
@@ -1,101 +1,27 @@
 package com.uni.vendas.data.dto.v1;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
-import org.springframework.hateoas.RepresentationModel;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
-import java.util.Objects;
+import java.util.UUID;
 
-//@JsonFilter("PasswordFilter")
 @JsonPropertyOrder({"id", "first_name", "last_name", "city", "address", "email", "phone"})
-public class UserDTO extends RepresentationModel<UserDTO> {
-
-    private static final long serialVersionUID = 1L;
-
-    private Long id;
-    @JsonProperty("first_name")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String firstName;
-    @JsonProperty("last_name")
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String lastName;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String email;
-    private String phone;
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String password;
-    private String city;
-
-    public UserDTO() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        if (!super.equals(o)) return false;
-        UserDTO userDTO = (UserDTO) o;
-        return Objects.equals(getId(), userDTO.getId()) && Objects.equals(getFirstName(), userDTO.getFirstName()) && Objects.equals(getLastName(), userDTO.getLastName()) && Objects.equals(getEmail(), userDTO.getEmail()) && Objects.equals(getPhone(), userDTO.getPhone()) && Objects.equals(getPassword(), userDTO.getPassword()) && Objects.equals(getCity(), userDTO.getCity());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(super.hashCode(), getId(), getFirstName(), getLastName(), getEmail(), getPhone(), getPassword(), getCity());
-    }
-}
+public record UserDTO(
+        UUID id,
+        @NotBlank
+        @JsonProperty("first_name")
+        String firstName,
+        @NotBlank
+        @JsonProperty("last_name")
+        String lastName,
+        @Email
+        String email,
+        @NotBlank
+        String phone,
+        @NotBlank
+        String password,
+        @NotBlank
+        String city
+) {}
