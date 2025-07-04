e55c7be8d69fd1121dfc1923440020bddbfd8968
KaweVK <victorkawe07@gmail.com>
2025-07-03 22:03:21 -0300

    Created DuplicatedRegisterException

---
+++ src/main/java/com/uni/vendas/exception/DuplicatedRegisterException.java
@@ -0,0 +1,9 @@
+package com.uni.vendas.exception;
+
+public class DuplicatedRegisterException extends  RuntimeException {
+
+    public DuplicatedRegisterException(String message) {
+        super(message);
+    }
+
+}
