3085da85a7d8a5216523f07efe7f92dc7f4fe09f
KaweVK <victorkawe07@gmail.com>
2025-07-03 22:06:12 -0300

    Created ItemValidator to validate item

---
+++ src/main/java/com/uni/vendas/validator/ItemValidator.java
@@ -0,0 +1,33 @@
+package com.uni.vendas.validator;
+
+import com.uni.vendas.exception.DuplicatedRegisterException;
+import com.uni.vendas.models.Item;
+import com.uni.vendas.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class ItemValidator {
+
+    private final ItemRepository itemRepository;
+
+    public void validate(Item item){
+        if(existItem(item)){
+            throw new DuplicatedRegisterException("Author alredy exists");
+        }
+    }
+
+    private boolean existItem(Item item) {
+        Optional<Item> itemFinded = itemRepository.findByIdAndNameAndDescriptionAndAmountAndPrice(item.getId(), item.getName(), item.getDescription(), item.getAmount(), item.getPrice());
+
+        if (item.getId() == null) {
+            return itemFinded.isPresent();
+        }
+
+        return itemFinded.isPresent() && !item.getId().equals(itemFinded.get().getId());
+    }
+
+}
