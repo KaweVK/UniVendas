fe1799e8da2798013f99c6af344aa4cae493162f
KaweVK <victorkawe07@gmail.com>
2025-07-15 15:07:06 -0300

    Created Item Mapper

---
+++ src/main/java/com/uni/vendas/mapper/ItemMapper.java
@@ -0,0 +1,14 @@
+package com.uni.vendas.mapper;
+
+import com.uni.vendas.data.dto.v1.ItemDTO;
+import com.uni.vendas.models.Item;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ItemMapper {
+
+    Item toEntity(ItemDTO itemDTO);
+
+    ItemDTO toDTO(Item item);
+
+}
