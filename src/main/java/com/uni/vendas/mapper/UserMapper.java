1fe07a39e0f9815832aa873615655f07e8f96c0b
KaweVK <victorkawe07@gmail.com>
2025-07-15 20:42:38 -0300

    Updated mapper

--- src/main/java/com/uni/vendas/mapper/UserMapper.java
+++ src/main/java/com/uni/vendas/mapper/UserMapper.java
@@ -1,5 +1,6 @@
 package com.uni.vendas.mapper;
 
+import com.uni.vendas.data.dto.RegisterUserDTO;
 import com.uni.vendas.models.User;
 import org.mapstruct.Mapper;
 
@@ -8,5 +9,7 @@ public interface UserMapper {
 
     User toEntity(UserDTO userDTO);
 
-    UserDTO toDTO(User user);
+    RegisterUserDTO toRegisterDTO(User user);
+
+    SearchUserDTO toSearchDTO(User user);
 }
