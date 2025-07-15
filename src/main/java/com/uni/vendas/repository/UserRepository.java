9a0b7a897e1e2e9f9314a9751626efb1238a7849
KaweVK <victorkawe07@gmail.com>
2025-07-15 15:06:08 -0300

    Deleted all of user to create again later

--- src/main/java/com/uni/vendas/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.uni.vendas.repository;
-
-import com.uni.vendas.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
