00d8e58e3f58c3a495495e6efec2a7f669e4b118
KaweVK <victorkawe07@gmail.com>
2025-07-15 22:43:37 -0300

    Add methods to use the controller

--- src/main/java/com/uni/vendas/controllers/UserController.java
+++ src/main/java/com/uni/vendas/controllers/UserController.java
@@ -1,4 +1,72 @@
 package com.uni.vendas.controllers;
 
+import com.uni.vendas.data.dto.DeafultUserDTO;
+import com.uni.vendas.data.dto.RegisterUserDTO;
+import com.uni.vendas.models.User;
+import com.uni.vendas.services.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<Object> findUserById(@PathVariable("id") String id) {
+        var user = userService.findById(id);
+        if (user.isPresent()) {
+            return ResponseEntity.ok().body(user);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<Object> createUser(@RequestBody @Valid RegisterUserDTO userDTO) {
+        User user = userService.createUser(userDTO);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(location).body("User created successfully with ID: " + user.getId());
+    }
+
+    @PutMapping(
+            value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+    )
+    public ResponseEntity<Object> updateUser(@PathVariable("id") String id, @RequestBody @Valid RegisterUserDTO userDTO) {
+        Optional<DeafultUserDTO> userOptional = userService.updateUser(id, userDTO);
+        if (userOptional.isPresent()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteUser(@PathVariable("id") String id) {
+        Optional<DeafultUserDTO> userOptional = userService.findById(id);
+        if (userOptional.isPresent()) {
+            userService.deleteUser(id);
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
 }
