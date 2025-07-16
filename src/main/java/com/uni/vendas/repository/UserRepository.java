09f675598b7665f641f08336fc13bb44a32675e5
KaweVK <victorkawe07@gmail.com>
2025-07-15 21:22:16 -0300

    New method

--- src/main/java/com/uni/vendas/repository/UserRepository.java
+++ src/main/java/com/uni/vendas/repository/UserRepository.java
@@ -1,9 +1,14 @@
 package com.uni.vendas.repository;
 
 import com.uni.vendas.models.User;
+import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+
+
+    Optional<User> findByEmail(@Email(regexp = "^[a-zA-Z0-9._%+-]+@dcx.ufpb.br$", message = "Invalid email format") String email);
 }
