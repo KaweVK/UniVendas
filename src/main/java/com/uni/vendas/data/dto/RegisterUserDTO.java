48684187f4744ee13bed18efdf13e2f1bd18df2f
KaweVK <victorkawe07@gmail.com>
2025-07-15 21:19:26 -0300

    Created dtos to user

---
+++ src/main/java/com/uni/vendas/data/dto/RegisterUserDTO.java
@@ -0,0 +1,18 @@
+package com.uni.vendas.data.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.UUID;
+
+public record RegisterUserDTO(
+        UUID id,
+        @NotBlank(message = "Name cannot be blank")
+        String name,
+        @NotBlank(message = "Email cannot be blank")
+        String email,
+        @NotBlank(message = "Password cannot be blank")
+        String password,
+        @NotBlank(message = "Phone Number cannot be blank")
+        String phoneNumber,
+        @NotBlank(message = "City cannot be blank")
+        String city) {}
