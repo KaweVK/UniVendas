791617c619ba6db82c0d89aeb6ee8e7e82aa4118
KaweVK <victorkawe07@gmail.com>
2025-07-15 22:35:18 -0300

    fix validations on users dtos

--- src/main/java/com/uni/vendas/data/dto/RegisterUserDTO.java
+++ src/main/java/com/uni/vendas/data/dto/RegisterUserDTO.java
@@ -1,18 +1,26 @@
 package com.uni.vendas.data.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
 public record RegisterUserDTO(
         UUID id,
         @NotBlank(message = "Name cannot be blank")
+        @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
         String name,
         @NotBlank(message = "Email cannot be blank")
+        @Size(min = 3, max = 100, message = "Email deve ter entre 3 e 100 caracteres")
+        @Email(regexp = "^[a-zA-Z0-9._%+-]+@dcx.ufpb.br$", message = "Invalid email format")
         String email,
         @NotBlank(message = "Password cannot be blank")
+        @Size(min = 3, max = 50, message = "Senha deve ter entre 3 e 50 caracteres")
         String password,
         @NotBlank(message = "Phone Number cannot be blank")
+        @Size(min = 3, max = 15, message = "Número de celular deve ter entre 3 e 15 caracteres")
         String phoneNumber,
         @NotBlank(message = "City cannot be blank")
+        @Size(min = 3, max = 50, message = "Cidade deve ter entre 3 e 50 caracteres")
         String city) {}
