ad4cec61521170bf35e29c7d9481adca489adce3
KaweVK <victorkawe07@gmail.com>
2025-07-15 21:26:52 -0300

    Created UserValidator

---
+++ src/main/java/com/uni/vendas/validator/UserValidator.java
@@ -0,0 +1,34 @@
+package com.uni.vendas.validator;
+
+import com.uni.vendas.exception.DuplicatedRegisterException;
+import com.uni.vendas.models.Item;
+import com.uni.vendas.models.User;
+import com.uni.vendas.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class UserValidator {
+
+    private final UserRepository userRepository;
+
+    public void validate(User user){
+        if(existItem(user)){
+            throw new DuplicatedRegisterException("Item alredy exists");
+        }
+    }
+
+    private boolean existItem(User user) {
+        Optional<User> userFinded = userRepository.findByEmail(user.getEmail());
+
+        if (user.getId() == null) {
+            return userFinded.isPresent();
+        }
+
+        return userFinded.isPresent() && !user.getId().equals(userFinded.get().getId());
+    }
+
+}
