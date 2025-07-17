36dfb31d822a7be89821df23a2598f5ff7691f1e
KaweVK <victorkawe07@gmail.com>
2025-07-17 16:12:28 -0300

    Add extends

--- src/main/java/com/uni/vendas/repository/ItemRepository.java
+++ src/main/java/com/uni/vendas/repository/ItemRepository.java
@@ -2,11 +2,12 @@ package com.uni.vendas.repository;
 
 import com.uni.vendas.models.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ItemRepository extends JpaRepository<Item, UUID> {
+public interface ItemRepository extends JpaRepository<Item, UUID>, JpaSpecificationExecutor<Item> {
     Optional<Item> findByIdAndNameAndDescriptionAndAmountAndPrice(UUID id, String name, String description, Long amount, BigDecimal price);
 }
