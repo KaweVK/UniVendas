5d6275041eb389b7529565b90a5ddfa1d653717e
KaweVK <victorkawe07@gmail.com>
2025-07-17 17:50:54 -0300

    Changed to lowerCase

--- src/main/java/com/uni/vendas/repository/specs/ItemSpecs.java
+++ src/main/java/com/uni/vendas/repository/specs/ItemSpecs.java
@@ -6,11 +6,11 @@ import org.springframework.data.jpa.domain.Specification;
 public class ItemSpecs {
 
     public static Specification<Item> nameLike(String name) {
-        return (root, query, cb) -> cb.like(cb.upper(root.get("name")), "%" + name.toUpperCase() + "%");
+        return (root, query, cb) -> cb.like(cb.lower(root.get("name")), "%" + name.toLowerCase() + "%");
     }
 
     public static Specification<Item> descriptionLike(String description) {
-        return (root, query, cb) -> cb.like(cb.upper(root.get("description")), "%" + description.toUpperCase() + "%");
+        return (root, query, cb) -> cb.like(cb.lower(root.get("description")), "%" + description.toLowerCase() + "%");
     }
 
     public static Specification<Item> priceEqual(Double priceLess, Double priceGreater) {
