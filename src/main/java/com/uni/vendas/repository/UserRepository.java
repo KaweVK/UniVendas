c2023ef0cd7ce998620cf91072b8617368e6f782
KaweVK <victorkawe07@gmail.com>
2025-07-17 20:25:59 -0300

    Add extends specification

--- src/main/java/com/uni/vendas/repository/UserRepository.java
+++ src/main/java/com/uni/vendas/repository/UserRepository.java
@@ -3,12 +3,12 @@ package com.uni.vendas.repository;
 import com.uni.vendas.models.User;
 import jakarta.validation.constraints.Email;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
-
+public interface UserRepository extends JpaRepository<User, UUID>, JpaSpecificationExecutor<User> {
 
     Optional<User> findByEmail(@Email(regexp = "^[a-zA-Z0-9._%+-]+@dcx.ufpb.br$", message = "Invalid email format") String email);
 }
