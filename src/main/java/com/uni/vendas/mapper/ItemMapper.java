3da2e7f49783cad7310760b6ba71b89df4b3f45d
KaweVK <victorkawe07@gmail.com>
2025-07-24 19:54:34 -0300

    Changed interface to abstract class to use a expression

--- src/main/java/com/uni/vendas/mapper/ItemMapper.java
+++ src/main/java/com/uni/vendas/mapper/ItemMapper.java
@@ -2,13 +2,20 @@ package com.uni.vendas.mapper;
 
 import com.uni.vendas.data.dto.ItemDTO;
 import com.uni.vendas.models.Item;
+import com.uni.vendas.repository.UserRepository;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper(componentModel = "spring")
-public interface ItemMapper {
+@Mapper(componentModel = "spring", uses = {UserMapper.class})
+public abstract class ItemMapper {
 
-    Item toEntity(ItemDTO itemDTO);
+    @Autowired
+    UserRepository userRepository;
 
-    ItemDTO toDTO(Item item);
+    @Mapping(target = "soldBy", expression = "java( userRepository.findById(itemDTO.soldById()).orElse(null) )")
+    public abstract Item toEntity(ItemDTO itemDTO);
+
+    public abstract ItemDTO toDTO(Item item);
 
 }
