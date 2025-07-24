0f8de344b1f7d2df3edfcb37964090b5f1fe967f
KaweVK <victorkawe07@gmail.com>
2025-07-24 19:50:37 -0300

    Created item category enum

---
+++ src/main/java/com/uni/vendas/models/enums/ItemCategory.java
@@ -0,0 +1,13 @@
+package com.uni.vendas.models.enums;
+
+public enum ItemCategory {
+    TECNOLOGIA,
+    MOVEL,
+    ELETRODOMESTICO,
+    CELULAR,
+    COMPUTADOR,
+    NOTEBOOK,
+    PAPELARIA,
+    COMIDA
+
+}
