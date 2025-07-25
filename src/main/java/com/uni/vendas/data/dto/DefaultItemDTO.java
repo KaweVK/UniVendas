e261c6696abcc44201aa15f0ae3f8bf134b02fcf
KaweVK <victorkawe07@gmail.com>
2025-07-24 21:17:54 -0300

    New dto to item

---
+++ src/main/java/com/uni/vendas/data/dto/DefaultItemDTO.java
@@ -0,0 +1,28 @@
+package com.uni.vendas.data.dto;
+
+import com.uni.vendas.models.enums.ItemCategory;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public record DefaultItemDTO(
+        UUID id,
+        @NotBlank(message = "Name cannot be blank")
+        @Size(min = 3, max = 100, message = "Nome deve ter entre 3 e 100 caracteres")
+        String name,
+        @NotBlank(message = "Description cannot be blank")
+        @Size(min = 20, max = 2000, message = "Descrição deve ter entre 20 e 2000 caracteres")
+        String description,
+        @NotNull(message = "Amount is required")
+        Long amount,
+        @NotNull(message = "Price is required")
+        BigDecimal price,
+        @NotNull(message = "User ID is required")
+        DefaultUserDTO soldBy,
+        @NotNull(message = "Category is required")
+        ItemCategory category
+) {
+}
