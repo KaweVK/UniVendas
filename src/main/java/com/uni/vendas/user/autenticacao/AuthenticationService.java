2e8eaa5da11cfa9bd0af44ff3367582f459422e5
KaweVK <victorkawe07@gmail.com>
2025-11-06 20:09:33 -0300

    New AuthenticationService

---
+++ src/main/java/com/uni/vendas/user/autenticacao/AuthenticationService.java
@@ -0,0 +1,20 @@
+package com.uni.vendas.user.autenticacao;
+
+import com.uni.vendas.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return (UserDetails) userRepository.findUserByEmail(username); //para achar o username do cara e ele
+    }
+}
