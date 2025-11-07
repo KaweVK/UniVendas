b04a6527d27b368d1b93661e42faa15bd4e1b769
KaweVK <victorkawe07@gmail.com>
2025-11-06 21:47:11 -0300

    New methods and implement

--- src/main/java/com/uni/vendas/user/models/User.java
+++ src/main/java/com/uni/vendas/user/models/User.java
@@ -8,8 +8,13 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -18,7 +23,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
-public class User {
+public class User implements UserDetails {
 
     @Id
     @Column(name = "id", updatable = false, nullable = false, unique = true)
@@ -42,4 +47,13 @@ public class User {
     @Column(name = "update_date")
     private LocalDateTime updateDate;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
