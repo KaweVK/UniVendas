50184a5a0082d89c2ab7213595e7a079e0b3669e
KaweVK <victorkawe07@gmail.com>
2025-11-06 21:45:47 -0300

    New AuthenticationController

---
+++ src/main/java/com/uni/vendas/user/autenticacao/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.uni.vendas.user.autenticacao;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @PostMapping
+    public ResponseEntity<Object> login(@RequestBody @Valid AuthenticantionDataDto dto) {
+        var token = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+        var auth = manager.authenticate(token);
+
+        return ResponseEntity.ok().build();
+    }
+
+}
