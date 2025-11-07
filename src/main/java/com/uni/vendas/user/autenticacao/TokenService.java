e4ba6e458f3317b7c944d184369e24770edd9883
KaweVK <victorkawe07@gmail.com>
2025-11-07 18:08:46 -0300

    Add TokenService

---
+++ src/main/java/com/uni/vendas/user/autenticacao/TokenService.java
@@ -0,0 +1,34 @@
+package com.uni.vendas.user.autenticacao;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.uni.vendas.user.models.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    public String generateToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256("kawe");
+            return JWT.create()
+                    .withIssuer("UniVendas")
+                    .withSubject(user.getEmail())
+                    .withClaim("email", user.getEmail())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar JWT", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
