81908367a8f32c60732e9c9f39455928021e109b
KaweVK <victorkawe07@gmail.com>
2025-11-12 18:25:46 -0300

    Update method

--- src/main/java/com/uni/vendas/infra/security/SecurityFilter.java
+++ src/main/java/com/uni/vendas/infra/security/SecurityFilter.java
@@ -1,29 +1,51 @@
 package com.uni.vendas.infra.security;
 
-import jakarta.servlet.Filter;
+import com.uni.vendas.user.autenticacao.TokenService;
+import com.uni.vendas.user.repository.UserRepository;
+import com.uni.vendas.user.services.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
+@AllArgsConstructor
 public class SecurityFilter extends OncePerRequestFilter {
+
+    private TokenService tokenService;
+    private UserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var tokenJWT = recuperarToken(request);
+
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var user = userRepository.findByEmail(subject);
+
+            var auth = new UsernamePasswordAuthenticationToken(user, null, user.get().getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+
         filterChain.doFilter(request, response);
     }
 
     private String recuperarToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
-        if (authHeader == null) {
-            throw new SecurityException("Authorization header missing token JWT");
+        if (authHeader != null) {
+            return authHeader.replace("Bearer ", "");
         }
-        return authHeader.replace("Bearer ", "");
+
+        return null;
     }
 }
