fbca285c643a5813b57dc9a8b03d012d6b9f691c
KaweVK <victorkawe07@gmail.com>
2025-11-12 17:22:13 -0300

    New method

--- src/main/java/com/uni/vendas/user/autenticacao/TokenService.java
+++ src/main/java/com/uni/vendas/user/autenticacao/TokenService.java
@@ -3,6 +3,8 @@ package com.uni.vendas.user.autenticacao;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.uni.vendas.infra.exception.InvalidTokenJWT;
 import com.uni.vendas.user.models.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -29,6 +31,20 @@ public class TokenService {
                     .sign(algorithm);
         } catch (JWTCreationException exception){
             throw new RuntimeException("Erro ao gerar JWT", exception);
+        }
+    }
+
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("UniVendas")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        } catch (JWTVerificationException exception){
+            throw new InvalidTokenJWT("Invalid JWT");
         }
     }
 
