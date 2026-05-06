3674feb7c36f107fe5cbc098e13124ca154802d6
KaweVK <victorkawe07@gmail.com>
2026-05-06 16:55:26 -0300

    Fix: Corrigindo css da lista suspensa e usando novo formatador

--- frontend/univendas/src/componentes/ListaSuspensa/index.jsx
+++ frontend/univendas/src/componentes/ListaSuspensa/index.jsx
@@ -1,13 +1,23 @@
-import './ListaSuspensa.css'
+import { formatarCategoria } from "../../utils/formatter";
 
 export const ListaSuspensa = (props) => {
     return (
-        <div className='lista-suspensa'>
-            <label>{props.label}</label>
-            <select required={props.obrigatorio} value={props.valor} onChange={evento => props.aoAlterado(evento.target.value)}>
-                <option value=" "></option>
-                {props.itens.map(item => <option key={item}>{item}</option>)}
+        <div>
+            <label htmlFor={props.id} className={props.labelClassName}>
+                {props.label}
+            </label>
+            <select
+                id={props.id}
+                required={props.obrigatorio}
+                value={props.valor}
+                onChange={evento => props.aoAlterado(evento.target.value)}
+                className={props.className}
+            >
+                <option value="">Selecione a Categoria</option>
+                {formatarCategoria(props.itens).map(item => {
+                    return <option key={item.valor} value={item.valor}>{item.rotulo}</option>;
+                })}
             </select>
         </div>
-    )    
+    )
 }
